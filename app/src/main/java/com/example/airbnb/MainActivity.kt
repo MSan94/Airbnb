01c65e3aaa8d67553dce2892d506f3b66864f6bb
MyeongSeong <ms1994s@naver.com>
2021-06-06 21:10:39 +0900

    BottomSheetDialog 예약 집 목록 추가

--- app/src/main/java/com/example/airbnb/MainActivity.kt
+++ app/src/main/java/com/example/airbnb/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.airbnb
 
 import android.graphics.Color
+import android.graphics.LinearGradient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 import com.naver.maps.map.util.MarkerIcons
+import com.naver.maps.map.widget.LocationButtonView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -28,7 +32,18 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         findViewById(R.id.houseViewPager)
     }
 
+    private val recyclerView : RecyclerView by lazy {
+        findViewById(R.id.recyclerView)
+    }
+
+    private val currentLocationButton : LocationButtonView by lazy {
+        findViewById(R.id.currentLocationButton)
+    }
+
     private val viewPagerAdapter = HouseViewPagerAdapter()
+    private val recyclerAdapter = HouseListAdapter()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +53,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         mapView.getMapAsync(this)
         viewPager.adapter = viewPagerAdapter
+        recyclerView.adapter = recyclerAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
     }
 
 
@@ -52,8 +70,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // 현 위치 버튼
         val uiSettings = naverMap.uiSettings
-        uiSettings.isLocationButtonEnabled =
-            true // 사용권한을 manifest에 정의 , API 2.6이후부턴 팝업을 통해 권한 동의 필요
+        uiSettings.isLocationButtonEnabled = false // 사용권한을 manifest에 정의 , API 2.6이후부턴 팝업을 통해 권한 동의 필요
+        currentLocationButton.map = naverMap
         locationSource = FusedLocationSource(this@MainActivity, LOCATION_PERMISSTION_REQUEST_CODE)
         naverMap.locationSource = locationSource
 
@@ -85,6 +103,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                                 marker.iconTintColor = Color.RED
                             }
                             viewPagerAdapter.submitList(dto.items)
+                            recyclerAdapter.submitList(dto.items)
                         }
                     }
 
